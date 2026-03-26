22cf09ce9fd1ee4a1a906b8e68a6f0fd1d1218be
Otoniel <252310356@itslerdo.edu.mx>
2026-03-25 18:42:40 -0600

    actividad_practica

---
+++ actividad_practica/Tienda.php
@@ -0,0 +1,37 @@
+<?php
+
+class Tienda {
+    // Método con parámetros y retorno
+ public function calcularDescuento($precio) {
+ if ($precio >= 1000) {
+ return $precio * 0.20;
+ } elseif ($precio >= 500) {
+ return $precio * 0.10;
+ } elseif ($precio < 0){
+    return 'El precio no puede ser negativo.';
+    } else {
+ return 0;
+ }
+ }
+ // Método con parámetros y retorno
+ public function calcularTotal($precio, $descuento) {
+    if (is_string($descuento)) {
+        return $descuento; // Retorna el mensaje de error si el descuento es un string
+    }
+ return $precio - $descuento;
+ }
+ // Método para mostrar resultados
+public function mostrarResultado($producto, $precio, $descuento, $total) {
+        echo "<div class='result-box'>";
+        echo "<h3>Resultado</h3>";
+        echo "<div class='item'>Producto: <strong>$producto</strong></div>";
+        echo "<div class='item'>Precio original: $$precio</div>";
+        echo "<div class='item'>Descuento: $$descuento</div>";
+        echo "<div class='total'>Total a pagar: $$total</div>";
+
+        if ($total == $precio && $precio > 0) {
+            echo "<p class='error'>No hay descuento disponible.</p>";
+        }
+        echo "</div>";
+    }
+}
